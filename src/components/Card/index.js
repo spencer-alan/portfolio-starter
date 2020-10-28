515f4907959e7f7c6c46a23f0f65062e394de16b
b1acksmyth <smith.spence13@gmail.com>
2020-10-28 16:56:29 -0700

    added components

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// All projects will be in card form under the Jumbotron of feature project
+function Card(props) {
+  return (
+    <div className="card">
+
+    </div>
+  )
+}
+
+export default Card;
