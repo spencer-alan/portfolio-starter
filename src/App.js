01fb40c56d23870511a440b752dad53218dbc71c
b1acksmyth <smith.spence13@gmail.com>
2020-10-29 17:11:15 -0700

    adde react routes to app and made pages to route too

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
 
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <Nav />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/contact" component={Contact} />
+      {/* <Route path="*" component="NoMatch" /> */}
+    </Router>
   );
 }
 
