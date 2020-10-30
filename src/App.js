119345803ee8ec7dca38a337fee5ad39e2e4c43f
b1acksmyth <smith.spence13@gmail.com>
2020-10-29 17:51:51 -0700

    brought in react router to app and to Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav/index";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
-
 function App() {
-  return (
-    <Router>
-      <Nav />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/contact" component={Contact} />
-      {/* <Route path="*" component="NoMatch" /> */}
-    </Router>
-  );
+	return (
+		<Router>
+			<Nav />
+			<Route exact path="/" component={Home} />
+			<Route exact path="/about" component={About} />
+			<Route exact path="/projects" component={Projects} />
+			<Route exact path="/contact" component={Contact} />
+			{/* <Route path="*" component="NoMatch" /> */}
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,89 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        Your personal CMS
-      </a>
-      {/* Display this if the current state is loading */}
-      {/* <a className="navbar-brand ml-auto">
-          Loading...
-        </a> */}
-    </nav>
-  );
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<Link className="navbar-brand" href="/">
+				Navbar
+			</Link>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span className="navbar-toggler-icon"></span>
+			</button>
+
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav mr-auto">
+					<li className="nav-item active">
+						<Link
+							to="/"
+							className={
+								window.location.pathname === "/"
+									? "nav-link active"
+									: "nav-link"
+							}
+						>
+							Home <span className="sr-only">(current)</span>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							to="/projects"
+							className={
+								window.location.pathname === "/projects"
+									? "nav-link active"
+									: "nav-link"
+							}
+						>
+							Projects
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							to="/about"
+							className={
+								window.location.pathname === "/about"
+									? "nav-link active"
+									: "nav-link"
+							}
+						>
+							About
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link
+							to="/contact"
+							className={
+								window.location.pathname === "/contact"
+									? "nav-link active"
+									: "nav-link"
+							}
+						>
+							Contact
+						</Link>
+					</li>
+				</ul>
+				<form className="form-inline my-2 my-lg-0">
+					<input
+						className="form-control mr-sm-2"
+						type="search"
+						placeholder="Search"
+						aria-label="Search"
+					/>
+					<button className="btn btn-outline-info my-2 my-sm-0" type="submit">
+						Search
+					</button>
+				</form>
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
