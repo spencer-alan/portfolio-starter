601b57f528248878fd973c5d8f02dbbbc7ed0bc2
b1acksmyth <smith.spence13@gmail.com>
2020-10-31 10:09:40 -0700

    renameing of files to be clearer about what they do

---
+++ src/components/Info/homeInfo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./homeInfoStyle.css";
+
+
+function HomeInfo() {
+  return (
+    <div className="info">
+      <p className="text-center">
+      Welcome to my portfolio! 
+      I am a Full stack web developer with public relations education and a certificate in full stack development from University of California Irvine, 
+      with an understanding of Javascript, CSS, Node.js, MySQL, Express, MongoDB, and React while still looking to expand my knowledge base. 
+      </p>
+      <br />
+      <p className="text-center">
+        Please check out my Projects page to see soem examples of my work and see my about page to get to know more about me!
+      </p>
+    </div>
+  )
+}
+
+export default HomeInfo;
