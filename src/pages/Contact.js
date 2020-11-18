54699b01d91e1bf3d52bfc319a2e2f558213a403
b1acksmyth <smith.spence13@gmail.com>
2020-11-17 16:23:42 -0800

    style update and basic contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
 import { Container } from "../components/Grid";
-import Form from "../components/Form/Form";
+import ContactInfo from "../components/Info/contactInfo";
 
 function Contact() {
   return (
     <>
       <Nav />
-      <Container>
-        <Form />
+      <Container style={{backgroundColor: "#778da9", borderRadius: "25px", height: "20rem"}}>
+        <ContactInfo />
       </Container>
     </>
   )
